bc8ddda41be2607a3a8eaa9403100119ac6b513d
Eb-Wan <ewanbaroneb.com@gmail.com>
2025-02-21 17:06:51 +0100

    Push de 17h

--- backend/controllers/userController.js
+++ backend/controllers/userController.js
@@ -36,7 +36,7 @@ exports.login = async (req, res, next) => {
         if (!passMatch) throw new Exeption("Wrong username/email or password", 400, true);
 
         const token = generateToken(user.id);
-        res.cookie("token", token, { httpOnly: true, maxAge: 12*60*60*1000 });
+        res.cookie("token", token, { sameSite: "strict", maxAge: 12*60*60*1000 });
         res.status(200).json({ success: true });
     } catch (error) {
         next(error);        
@@ -71,7 +71,7 @@ exports.register = async (req, res, next) => {
 
         const token = generateToken(user.id);
 
-        res.cookie("token", token, { httpOnly: true, maxAge: 12*60*60*1000 });
+        res.cookie("token", token, { sameSite: "strict", maxAge: 12*60*60*1000 });
         res.status(201).json({ success: true });
     } catch (error) {
         if (error.code && error.code === 11000) error = new Exeption("This user already exists", 400, true)
