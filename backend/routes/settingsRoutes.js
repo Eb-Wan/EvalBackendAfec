9745df130b243a5a617001fbef22019c89f16571
Eb-Wan <ewanbaroneb.com@gmail.com>
2025-02-21 12:52:27 +0100

    Merge

--- backend/routes/settingsRoutes.js
+++ backend/routes/settingsRoutes.js
@@ -1,8 +1,8 @@
 const express = require("express");
 const router = express.Router();
-const { authMiddleware, isAdmin } = require("../middlewares/protectMiddleware");
+const { authMiddleware } = require("../middlewares/protectMiddleware");
 const { validateSettings, validateRequest } = require("../middlewares/validatorMiddleware");
-const { getSettings, update, remove } = require("../controllers/settingsController");
+const { getSettings, update } = require("../controllers/settingsController");
 
 router.get("/", authMiddleware, getSettings);
 router.put("/", authMiddleware, validateSettings, validateRequest, update);
